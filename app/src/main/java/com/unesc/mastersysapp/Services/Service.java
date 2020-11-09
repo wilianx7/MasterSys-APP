1cc2148754e3834ac360421eaeed1a0ce197a845
Giovane <giovaneleacina@unesc.net>
2020-11-08 21:48:55 -0300

    Refatoração

--- app/src/main/java/com/unesc/mastersysapp/Services/Service.java
+++ app/src/main/java/com/unesc/mastersysapp/Services/Service.java
@@ -1,5 +1,5 @@
 package com.unesc.mastersysapp.Services;
 
 public class Service {
-    public static final String BASE_URL = "http://192.168.0.104:8000/";
+    public static final String BASE_URL = "http://localhost:8000/";
 }
